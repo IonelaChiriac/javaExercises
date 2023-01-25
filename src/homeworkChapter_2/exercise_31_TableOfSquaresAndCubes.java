a188528ff6e671234a1175a67800496ce6e256fc
IonelaChiriac <ionela.s.chiriac@gmail.com>
2023-01-25 10:21:06 +0200

    ex chapt 2

---
+++ src/homeworkChapter_2/exercise_31_TableOfSquaresAndCubes.java
@@ -0,0 +1,67 @@
+package homeworkChapter_2;
+
+import java.util.Scanner;
+
+//(Table of Squares and Cubes)
+// write an application that calculates the squares and cubes of the numbers from 0 to 10 and
+//prints the resulting values in table format.
+/*
+    number square cube
+        0    0     0
+        1    1     1
+        2    4     8
+        3    9     27
+        4    16    64
+        5    25    125
+        6    36    216
+        7    49    343
+        8    64    512
+        9    81    729
+        10   100   1000
+*/
+
+public class exercise_31_TableOfSquaresAndCubes {
+
+    // main method begins execution of Java application
+    public static void main(String[] args)
+    {
+        System.out.println("number   square   cube");
+        int firstNumber = 0;
+        System.out.printf("%d         %d       %d%n", firstNumber, firstNumber * firstNumber, firstNumber * firstNumber * firstNumber);
+        int secondNumber = 1;
+        System.out.printf("%d         %d       %d%n", secondNumber, secondNumber * secondNumber, secondNumber * secondNumber * secondNumber);
+        int thirdNumber = 2;
+        System.out.printf("%d         %d       %d%n", thirdNumber, thirdNumber * thirdNumber, thirdNumber * thirdNumber * thirdNumber);
+        int fourthNumber = 3;
+        System.out.printf("%d         %d       %d%n", fourthNumber, fourthNumber * fourthNumber, fourthNumber * fourthNumber * fourthNumber);
+        int fifthNumber = 4;
+        System.out.printf("%d         %d      %d%n", fifthNumber, fifthNumber * fifthNumber, fifthNumber * fifthNumber * fifthNumber);
+        int sixthNumber = 5;
+        System.out.printf("%d         %d      %d%n", sixthNumber, sixthNumber * sixthNumber, sixthNumber * sixthNumber * sixthNumber);
+        int seventhNumber = 6;
+        System.out.printf("%d         %d      %d%n", seventhNumber, seventhNumber * seventhNumber, seventhNumber * seventhNumber * seventhNumber);
+        int eightNumber = 7;
+        System.out.printf("%d         %d      %d%n", eightNumber, eightNumber * eightNumber, eightNumber * eightNumber * eightNumber);
+        int ninthNumber = 8;
+        System.out.printf("%d         %d      %d%n", ninthNumber, ninthNumber * ninthNumber, ninthNumber * ninthNumber * ninthNumber);
+        int tenthNumber = 9;
+        System.out.printf("%d         %d      %d%n", tenthNumber, tenthNumber * tenthNumber, tenthNumber * tenthNumber * tenthNumber);
+        int eleventhNumber = 10;
+        System.out.printf("%d        %d     %d%n", eleventhNumber, eleventhNumber * eleventhNumber, eleventhNumber * eleventhNumber * eleventhNumber);
+    }
+}
+
+//output
+//
+//        number   square   cube
+//        0         0       0
+//        1         1       1
+//        2         4       8
+//        3         9       27
+//        4         16      64
+//        5         25      125
+//        6         36      216
+//        7         49      343
+//        8         64      512
+//        9         81      729
+//        10        100     1000
---
+++ src/homeworkChapter_2/exercise_33_BodyMassIndexCalculator.java
@@ -0,0 +1,57 @@
+package homeworkChapter_2;
+import java.util.Scanner;
+
+//(Body Mass Index Calculator)
+// We introduced the body mass index (BMI) calculator in Exercise 1.10.
+//The formulas for calculating BMI are
+//BMI = weightInPounds × 703 / (heightInInches * heightInInches)
+//or
+//BMI = weightInKilograms / heightInMeters * heightInMeters
+//Create a BMI calculator that reads the user’s weight in pounds and height in inches (or, if you prefer, the user’s weight in kilograms and height in meters),
+// then calculates and displays the user’s body mass index.
+// Also, display the following information from the Department of Health and
+//Human Services/National Institutes of Health so the user can evaluate his/her BMI:
+//BMI VALUES
+//Underweight: less than 18.5
+//Normal: between 18.5 and 24.9
+//Overweight: between 25 and 29.9
+//Obese: 30 or greater
+
+public class exercise_33_BodyMassIndexCalculator {
+
+
+    // main method begins execution of Java application
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double weightInKilograms;
+        double heightInMeters;
+        double bodyMassIndex;
+
+        System.out.println("Please enter weight in kilograms: ");
+        weightInKilograms = input.nextDouble();
+
+        System.out.println("Please enter height in meters: ");
+        heightInMeters = input.nextDouble();
+
+        bodyMassIndex = weightInKilograms / (heightInMeters * heightInMeters);
+        System.out.println("Your BMI is " + bodyMassIndex);
+
+        System.out.println("\n BMI Values \n Underweight: less than 18.5 \n Normal:    between 18.5 and 24.9 \n Overweight:  between 25 and 29.9 \n Obese:    30 or greater");
+
+
+    }
+}
+
+//output
+//        Please enter weight in kilograms:
+//        53
+//        Please enter height in meters:
+//        1.58
+//        Your BMI is 21.230572023714146
+//
+//        BMI Values
+//        Underweight: less than 18.5
+//        Normal:    between 18.5 and 24.9
+//        Overweight:  between 25 and 29.9
+//        Obese:    30 or greater
